ef16ea798ebb84124ca90c2d3a2f5ad1975ba742
luishck <luisjcenteno@live.com>
2017-04-15 22:08:18 -0600

    New Features -Loading feedback -Pull to refresh -Alerts

--- src/pages/ventas/ventas.ts
+++ src/pages/ventas/ventas.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
-import {HttpProvider} from '../../providers/http-provider';
+import { NavController, AlertController } from 'ionic-angular';
+import { HttpProvider } from '../../providers/http-provider';
+import { LoaderCrtl } from "../../providers/loader";
 import { Storage } from '@ionic/storage';
 
 import { SaleDetails } from '../sale-details/sale-details';
+import { HomePage } from "../home/home";
 
 /**
  * Generated class for the Ventas page.
@@ -14,36 +16,65 @@ import { SaleDetails } from '../sale-details/sale-details';
 @Component({
   selector: 'page-ventas',
   templateUrl: 'ventas.html',
-  providers:[HttpProvider]
+  providers: [HttpProvider, LoaderCrtl]
 })
 export class Ventas {
-  
-  listventas:any;
-  
-  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, public storage: Storage) {
-    this.storage.get('token').then(token=>{
-          this.listSales(token);  
+
+  listventas: any;
+  token: any;
+
+  constructor(public navCtrl: NavController, private httpProvider: HttpProvider, public storage: Storage, public loaderCrtl: LoaderCrtl, public alertCtrl: AlertController) {
+    this.storage.get('token').then(token => {
+      this.listSales(token);
     })
   }
 
-  saleDetails(id){
-    this.navCtrl.push(SaleDetails,{
+  saleDetails(id) {
+    this.navCtrl.push(SaleDetails, {
       param_id: id
     });
   }
 
-  listSales(token){
-  this.httpProvider.getData(token, 'sales').subscribe(
-    result => {
-      this.listventas=result;
-      console.log("Success : "+this.listventas);
-    },
-    err =>{
-      console.error("Error : "+err);
-    } ,
-    () => {
-      console.log('getData completed');
-    }
-  );
+  showAlert(title, message) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: [        
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.push(HomePage)
+          }
+        }]
+    });
+    alert.present();
+  }
+
+  doRefresh(refresher) {
+    this.httpProvider.getData(this.token, 'sales').subscribe(
+      result => {
+        this.listventas = result;
+        if (refresher != 0) {
+          refresher.complete();
+        }
+      });
+  };
+
+  listSales(token) {
+    this.loaderCrtl.startLoader()
+    this.httpProvider.getData(token, 'sales').subscribe(
+      result => {
+        this.listventas = result;
+        console.log("Success : " + this.listventas);
+      },
+      err => {
+        this.showAlert("Error","No se pudo cargar la lista de ventas")
+        this.loaderCrtl.stopLoader();
+      },
+      () => {
+        this.loaderCrtl.stopLoader();
+      }
+    );
+    this.token = token;
   }
 }
