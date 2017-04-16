639571d54bf688bfa34f0b5cd517dbedfcefb0ec
luishck <luisjcenteno@live.com>
2017-04-15 22:02:35 -0600

    Import pages and create alert function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import {HttpProvider} from '../../providers/http-provider';
 import { Storage } from '@ionic/storage';
+import { LoaderCrtl } from "../../providers/loader";
 
 import { Ventas } from '../ventas/ventas';
 import { Services } from '../services/services';
 import { Cash } from '../cash/cash';
+import { Invoices } from '../invoices/invoices';
+import { Login } from "../login/login";
 
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers:[HttpProvider]
+  providers:[HttpProvider, LoaderCrtl]
 })
 export class HomePage {
   ventas = Ventas;
   services = Services;
   cash = Cash;
+  invoices = Invoices;
   
   sales_count: any;
   services_count: any;
   cash_total: any;
 
-  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, public loadingCtl: LoadingController, public storage: Storage) {
+  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, public loadingCtl: LoaderCrtl, public storage: Storage, public alertCtrl: AlertController) {
     this.storage.get('token').then(token=>{
           this.getCount(token);  
     })
   }
 
+  showAlert(title, message) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: [        
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.push(Login)
+          }
+        }]
+    });
+    alert.present();
+  }
+
   getCount(token){
-    console.log("token ventas:" + token)
+    this.loadingCtl.startLoader();
     this.httpProvider.getData(token, '').subscribe(
     result => {
       this.sales_count=result.sales;
@@ -38,10 +57,12 @@ export class HomePage {
       console.log("Success : ");
     },
     err =>{
-      console.error("Error : "+err);
+      this.showAlert("Error", "No se pudo conectar con el servidor, por favor vuelva a intentarlo")
+      this.loadingCtl.stopLoader()
     } ,
     () => {
       console.log('getData completed');
+      this.loadingCtl.stopLoader()
     });
   }
 }
